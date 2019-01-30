33728b6c08a8bc985edba33e36aeb6bffec6b594
gillsinaglass <rdgill95@gmail.com>
2019-01-30 16:12:05 -0500

    Show, index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < Sinatra::Base
 
+  set :views, 'app/views'
+  set :method_overide, true
+
+  get '/' do
+  end
+
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,19 @@
+class UserController < ApplicationController
+
+
+  get '/' do
+    redirect "/users"
+  end
+
+  get '/users' do
+    @all_users = User.all
+    erb :index
+  end
+
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :show_users
+  end
+
+
+end
