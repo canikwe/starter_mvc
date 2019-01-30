57144a3b435b4bc47d7a939ba0f0a10a8546635e
canikwe <c.anikwe@gmail.com>
2019-01-30 16:31:26 -0500

    updated user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,6 +3,15 @@ class UsersController < ApplicationController
   get '/' do
     redirect "/users"
   end
+  
+  get "/users/new" do
+    erb :"users/new"
+  end
+
+  post "/users" do
+    user = User.create(params)
+    redirect "/users/#{user.id}"
+  end
 
   get '/users' do
     @all_users = User.all
@@ -13,6 +22,5 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     erb :show_users
   end
-
 
 end
