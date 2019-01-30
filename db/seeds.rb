d76f0306e7ceb35d16b07ae6eb6369034be86f46
gillsinaglass <rdgill95@gmail.com>
2019-01-30 15:15:22 -0500

    Seed Data

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+ryan = User.create(name: "Ryan", description: "Spiritual guru guiding lost souls seaking enlightenment. Big fan of canned meat.")
+chine = User.create(name: "Chine", description: "Super tenacious, former teacher of Korea. Unfortunately doesn't like asian food.")
+emily = User.create(name: "Emily", description: "Has won best-laugh award 5 years in a row.")
+
+ben_kingdom = Host.create(name: "Ben's Kingdom", location: "Tampa, Florida", description: "Previously medieval-themed putt-putt. One free game per day of stay. Some rats.", base_price: 200.00, url: "http://www.scandiafun.com/images/golf_castle.jpg")
+
+james_wall = Host.create(name: "Jame's Wall House", location: "Denver, Colorado", description: "29,000 sq feet of rock-wall. Each room contains a bed at the top of a 50 ft climb. Harnesses not included.", base_price: 75.00, url: "https://cbsla.files.wordpress.com/2016/01/rockreation2.jpeg")
+
+paul_tiny_home = Host.create(name: "Paul's Tiny Home", location: "Des Moines, Iowa", description: "Quaint tiny-home, good for groups of friends that don't mind constantly saying excuse me while moving about the house. Come see all four things Des Moines has to offer!", base_price: 100.00, url: "http://www.billfrymire.com/blog/wp-content/uploads/2013/08/small-house-upgrade-home-size-H.jpg")
