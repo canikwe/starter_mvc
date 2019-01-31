d97cb16f002c60f94a3efe7d84d917985d1a4986
canikwe <c.anikwe@gmail.com>
2019-01-30 22:15:03 -0500

    Added reservation create and show and updated reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,12 +11,25 @@ class ReservationsController < ApplicationController
     erb :"reservations/new"
   end
 
+  post "/reservations" do
+    user = User.find(params[:user_id])
+    host = Host.find(params[:host][:id])
+    rez = Reservation.create(length_of_stay: params[:host][:length_of_stay],
+    host: host, user: user)
+    rez.cost = rez.final_cost
+    rez.save
+    redirect "/reservations/#{rez.id}"
+  end
   
-
   get '/reservations/:id' do
     @reservation = Reservation.find(params[:id])
     erb :"reservations/show"
   end
 
+  delete '/reservations/:id' do
+    rez = Reservation.find(params[:id])
+    rez.delete
+    redirect "/reservations/new"
+  end
 
 end
