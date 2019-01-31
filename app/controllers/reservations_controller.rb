0a73b4a4d8a1bd55966255ad904831734878db92
canikwe <c.anikwe@gmail.com>
2019-01-30 21:06:06 -0500

    added create  reservation form

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,9 +5,18 @@ class ReservationsController < ApplicationController
     erb :"reservations/index"
   end
 
+  get "/reservations/new" do
+    @users = User.all
+    @hosts = Host.all
+    erb :"reservations/new"
+  end
+
+  
+
   get '/reservations/:id' do
     @reservation = Reservation.find(params[:id])
     erb :"reservations/show"
   end
+
 
 end
